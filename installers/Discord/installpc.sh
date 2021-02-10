64b2f320e43bc091fedc69a5ef1141b603766840
SlippingGitty <76500838+SlippingGitty@users.noreply.github.com>
2021-02-09 20:03:44 -0600

    Add files via upload

---
+++ installers/Discord/installpc.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+PS3='Powercord Setup: '
+options=("Git Clone" "Install" "Uninstall" "Quit")
+select choice in "${options[@]}"; do
+    case $choice in
+        "Git Clone")
+            git clone https://github.com/powercord-org/powercord
+	    # Downloads Powercord files
+            ;;
+        "Install")
+            cd powercord && sudo npm run plug 
+	    # Injects mod into Canary
+            ;;
+        "Uninstall")
+            cd powercord && sudo npm run unplug
+	    # Uninjects mod
+	    break
+            ;;
+	"Quit")
+	    echo "User requested exit"
+	    exit
+	    ;;
+        *) echo "invalid option $REPLY";;
+    esac
+done
