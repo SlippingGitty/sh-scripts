f8e211a1c6a92c7e46cb3af740473c19f14996e6
SlippingGitty <76500838+SlippingGitty@users.noreply.github.com>
2021-02-10 17:47:26 -0600

    New dialog box, can update Vizality to latest version

--- installers/Discord/installviz.sh
+++ installers/Discord/installviz.sh
@@ -1,24 +1,33 @@
 #!/bin/bash
-cd $HOME
+dialog  --title "Vizality Setup" --infobox "Welcome. \n\nIf this is your first time running this script, download the Vizality repo by selecting option 1 first.\n\nAfterwards, select a function by typing the number that co-relates with the specific action you want to do." 10 70;sleep 5 
+clear
+cd
 PS3='Vizality Setup: '
-options=("Git Clone" "Install" "Uninstall" "Quit")
+options=("Git Clone" "Update" "Install" "Uninstall" "Quit")
 select choice in "${options[@]}"; do
     case $choice in
         "Git Clone")
-            git clone https://github.com/vizality/vizality
-	    # Git clones Vizality
+            git clone https://github.com/Vizality/Vizality
+	    # Downloads latest version of Vizality
+            ;;
+        "Update")
+            cd vizality && git pull
+            cd
+	    # Updates vizality to latest version
             ;;
         "Install")
-            cd vizality && sudo npm run inject 
+            cd vizality && sudo npm run inject
+            cd
 	    # Injects mod into Discord
             ;;
         "Uninstall")
             cd vizality && sudo npm run uninject
+            cd
 	    # Uninjects mod
-	    break
             ;;
 	"Quit")
 	    echo "User requested exit"
+	    exit
 	    ;;
         *) echo "invalid option $REPLY";;
     esac
