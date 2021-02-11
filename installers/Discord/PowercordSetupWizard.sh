8c68d7d2aee1595438c9ed5ce75841d1e47de77d
SlippingGitty <76500838+SlippingGitty@users.noreply.github.com>
2021-02-11 16:14:24 -0600

    commenting and organization

--- installers/Discord/PowercordSetupWizard.sh
+++ installers/Discord/PowercordSetupWizard.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
+#         ____  ____________       __
+#        / __ \/ ____/ ___/ |     / /
+#       / /_/ / /    \__ \| | /| / / 
+#      / ____/ /___ ___/ /| |/ |/ /  
+#     /_/    \____//____/ |__/|__/   
+#            VERSION 2021.02.11.01
+
+#INITIAL INFO PROMPT
 dialog  --title "Powercord Setup Wizard" --infobox "Welcome. \n\nIf this is your first time running this script, download the Powercord repo by selecting option 1 first.\n\nAfterwards, select a function by typing the number that co-relates with the specific action you want to do." 10 70;sleep 5 
+
+#DIALOG/NCURSES
 HEIGHT=15
 WIDTH=40
 CHOICE_HEIGHT=4
 BACKTITLE="Powercord Setup Wizard"
 TITLE="Installer"
 MENU="Select an option:"
-
-OPTIONS=(1 "Git Clone"
-         2 "Update"
-         3 "Install"
-         4 "Uninstall"
-         5 "Quit")
 
 while [ "$CHOICE -ne 4" ]; do
     CHOICE=$(dialog --clear \
@@ -22,6 +26,14 @@ while [ "$CHOICE -ne 4" ]; do
                 "${OPTIONS[@]}" \
                 2>&1 >/dev/tty)
 
+#OPTIONS NAMES
+OPTIONS=(1 "Git Clone"
+         2 "Update"
+         3 "Install"
+         4 "Uninstall"
+         5 "Quit")
+
+#OPTIONS IN EXECUTION
 clear
     case $CHOICE in
             1)  
