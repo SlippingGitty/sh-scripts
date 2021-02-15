b15df4dd8e64ccac751440fa11bb4522753686b4
SlippingGitty <76500838+SlippingGitty@users.noreply.github.com>
2021-02-15 16:33:02 -0600

    actually injects this time, commented functions, ascii art

--- installers/Discord/VizalitySetupWizard.sh
+++ installers/Discord/VizalitySetupWizard.sh
@@ -1,17 +1,25 @@
 #!/bin/bash
+#
+#    ██╗   ██╗███████╗██╗    ██╗
+#    ██║   ██║██╔════╝██║    ██║
+#    ██║   ██║███████╗██║ █╗ ██║
+#    ╚██╗ ██╔╝╚════██║██║███╗██║
+#     ╚████╔╝ ███████║╚███╔███╔╝
+#      ╚═══╝  ╚══════╝ ╚══╝╚══╝ 
+#       VERSION 2021.02.15.01
+
+#INITIAL INFO PROMPTS
 dialog  --title "Vizality Setup Wizard" --infobox "Welcome. \n\nIf this is your first time running this script, download the Vizality repo by selecting option 1 first.\n\nAfterwards, select a function by typing the number that co-relates with the specific action you want to do." 10 70;sleep 5 
+dialog --title  "READ THIS" --msgbox "Before injecting Vizality into your Discord client, please note that the dependencies must be installed first.\n\nAfter selecting Git Clone, please Install Dependencies, then Install." 10 60
+
+
+#DIALOG/NCURSES
 HEIGHT=15
 WIDTH=40
 CHOICE_HEIGHT=4
 BACKTITLE="Vizality Setup Wizard"
 TITLE="Installer"
 MENU="Select an option:"
-
-OPTIONS=(1 "Git Clone"
-         2 "Update"
-         3 "Install"
-         4 "Uninstall"
-         5 "Quit")
 
 while [ "$CHOICE -ne 4" ]; do
     CHOICE=$(dialog --clear \
@@ -22,6 +30,15 @@ while [ "$CHOICE -ne 4" ]; do
                 "${OPTIONS[@]}" \
                 2>&1 >/dev/tty)
 
+#OPTIONS NAMES
+OPTIONS=(1 "Git Clone"
+         2 "Update"
+         3 "Install Dependencies"
+         4 "Install"
+         5 "Uninstall"
+         6 "Quit")
+
+#OPTIONS IN EXECUTION
 clear
     case $CHOICE in
             1)  
@@ -35,16 +52,21 @@ clear
             # Updates Vizality to latest version
                 ;;
             3)
-                cd vizality && sudo npm run inject
+                cd vizality && npm install --only=prod --legacy-peer-deps
+                cd
+            # Installs dependencies
+                ;;
+            4)
+                cd vizality && sudo node src/inject inject --no-exit-codes 
                 cd
             # Injects Vizality into Discord
                 ;;
-            4)
+            5)
                 cd vizality && sudo npm run uninject
                 cd
             # Uninjects
                 ;;
-            5)
+            6)
             echo "User requested exit"
             exit
                 ;;
